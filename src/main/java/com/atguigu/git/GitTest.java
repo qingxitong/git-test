3663b71bca694793ed3e4447daaaf131cd242959
detonqing <detonqing@163.com>
2022-08-07 16:34:27 +0800

    1.2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.print("hello git");
         System.out.print("hello git2");
+        System.out.print("hello git3");
+        System.out.print("hello git3");
     }
 }
